0376ab154ea892241d336378ae4cdc8914f40a1f
Kunal Makwana <kunalm039@gmail.com>
2023-05-17 04:41:52 +0000

    homepage html added

--- furniture/src/App.js
+++ furniture/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import Admin from './components/admin';
+import React,{useState,useEffect} from 'react';
+// import Admin from './components/admin';
 
-const app = ()=>{
+const App = ()=>{
+  const [htmlFileString, setHtmlFileString] = useState(null);
 
+  async function fetchHtml() {
+    setHtmlFileString(await (await fetch(`index.html`)).text());
+  }
+  useEffect(() => {
+    fetchHtml();
+  }, []);
   return (
-    <Admin/>
+    // <Admin/>
+    <div className="App">
+      <div dangerouslySetInnerHTML={{ __html: htmlFileString }}></div>
+    </div>
   )
 }
-export default app;
+export default App;
