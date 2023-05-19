769350f226d3c5b706b3698f70c45ed2fe799934
Kunal Makwana <kunalm039@gmail.com>
2023-05-19 05:34:04 +0000

    admin add completed

--- furniture/src/App.js
+++ furniture/src/App.js
@@ -1,13 +1,13 @@
 import React,{useState,useEffect} from 'react';
-// import Admin from './components/admin';
+import Admin from './components/admin';
 import FurnitureDisplay from './components/products';
 
 const App = ()=>{
   return (
     // <Admin/>
     <div className="App">
-    <FurnitureDisplay/>
-
+    {/* <FurnitureDisplay/> */}
+    <Admin/>
     </div>
   )
 }
--- furniture/src/components/admin.js
+++ furniture/src/components/admin.js
@@ -67,44 +67,170 @@ const LoginForm = () => {
 
 // Create a component to display the home page
 const HomePage = () => {
-  const [images, setImages] = useState([]);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [traits, setTraits] = useState('');
+  const [category, setCategory] = useState('');
+  const [image, setImage] = useState(null);
+  const [products, setProducts] = useState([]);
 
-  // Get all of the images from Firebase
-  // const getImages = async () => {
-  //   try {
-  //     const query = db.collection("furniture-base");
-  //     const snapshot = await query.get();
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-  //     // Set the images state to the results of the query
-  //     setImages(snapshot.docs.map((doc) => doc.data()));
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
 
-  // // Listen for changes to the images collection
-  // db.collection("furniture-base").onSnapshot((snapshot) => {
-  //   setImages(snapshot.docs.map((doc) => doc.data()));
-  // });
+  const handleTraitsChange = (event) => {
+    setTraits(event.target.value);
+  };
 
-  // Render a list of images
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setImage(file);
+  };
+
+  const handleAddProduct = () => {
+    const newProduct = {
+      name,
+      price,
+      traits,
+      category,
+      image,
+    };
+
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+
+    // Clear input fields and image after adding product
+    setName('');
+    setPrice('');
+    setTraits('');
+    setCategory('');
+    setImage(null);
+  };
+  const handleDeleteProduct = (index) => {
+    setProducts((prevProducts) => {
+      const updatedProducts = [...prevProducts];
+      updatedProducts.splice(index, 1);
+      return updatedProducts;
+    });
+  };
+
+
   return (
     <div>
-      <h1>Home Page</h1>
-      {/* {images.map((image) => (
-        <img src={image.url} key={image.id} alt="nrih"/>
-      ))}
-      <button onClick={getImages}>Get Imagesd</button> */}
+      <h1>Product List</h1>
+      <div class="container">
+  <div class="row">
+    <div class="row-6">
+      <form className="row row-cols-1 row-cols-md-6 g-4">
+        <div class="mb-3 mr-3">
+          <label for="name" class="form-label">Name:</label>
+          <input
+            type="text"
+            id="name"
+            class="form-control"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div class="mb-3 mr-3">
+          <label for="price" class="form-label">Price:</label>
+          <input
+            type="text"
+            id="price"
+            class="form-control"
+            value={price}
+            onChange={handlePriceChange}
+          />
+        </div>
+        <div class="mb-3 mr-3">
+          <label for="traits" class="form-label">Traits:</label>
+          <input
+            type="text"
+            id="traits"
+            class="form-control"
+            value={traits}
+            onChange={handleTraitsChange}
+          />
+        </div>
+        <div class="mb-3 mr-3">
+          <label for="category" class="form-label">Category:</label>
+          <input
+            type="text"
+            id="category"
+            class="form-control"
+            value={category}
+            onChange={handleCategoryChange}
+          />
+        </div>
+        <div class="mb-3 mr-1">
+          <label for="image" class="form-label">Image:</label>
+          <input
+            type="file"
+            id="image"
+            class="form-control"
+            onChange={handleImageChange}
+          />
+        </div>
+        <button
+          type="button"
+          onClick={handleAddProduct}
+          class="btn btn-primary"
+        >
+          Add Product
+        </button>
+      </form>
+    </div>
+  </div>
+</div>
+      <h2>Product List:</h2>
+      <div className="row row-cols-1 row-cols-md-4 g-4 " >
+        {products.map((product, index) => (
+          <div className="col" key={index}>
+            <div className="card border-primary border-2" style={{ width: "304px"}} >
+              {product.image && (
+                <img
+                  src={URL.createObjectURL(product.image)}
+                  alt={product.name}
+                  className="card-img-top"
+                  style={{ objectFit: "cover", width: "300px", height: "300px" }}
+                />
+              )}
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">Price: {product.price}</p>
+                <p className="card-text">Traits: {product.traits}</p>
+                <button
+                  type="button"
+                  onClick={() => handleDeleteProduct(index)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
+
+
 };
 
 
   return (
     <div>
       
-      {user? null:<LoginForm />}
-      {user? <HomePage />:null}
+      {/* {user? null:<LoginForm />} */}
+      {/* {user? <HomePage />:null} */}
+      <HomePage/>
     </div>
   );
 };
